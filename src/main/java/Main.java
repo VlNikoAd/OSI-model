91e8833a220d2ad2a2723e4c787225897d792b3a
Vladislav <nikolaenko_vladislav@icloud.com>
2022-07-22 09:57:09 +0300

    add try/resourses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,16 @@ public class Main {
 
         try(ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+               try (Socket clientSocket = serverSocket.accept()) {
+                   PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                   BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                System.out.printf("New connection accept, Port: %d%n", clientSocket.getPort());
+                   System.out.printf("New connection accept, Port: %d%n", clientSocket.getPort());
 
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                   final String name = in.readLine();
+                   out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+               }
             }
         }
     }
